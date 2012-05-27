c13251cb41f6c29d7681b7d5bf02164de028212a
Tomohiro Nakamura <quickness.net@gmail.com>
2012-05-27 17:38:04 +0900

    roleが動くようになった

--- sphinxcontrib/spam.py
+++ sphinxcontrib/spam.py
@@ -12,7 +12,27 @@ u'''
 from sphinx.util.compat import Directive, make_admonition
 from docutils import nodes
 
-SPAM = 'spam! spam! spam!'
+SPAM = 'spam!'
+SPAMS = 'spam! spam! spam!'
+
+
+def spam_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    u'''chang str to "spam! spam! spam!".
+
+    Returns 2 part tuple containing list of nodes to insert into the
+    document and a list of system messages.  Both are allowed to be
+    empty.
+
+    :param name: The role name used in the document.
+    :param rawtext: The entire markup snippet, with role.
+    :param text: The text marked with the role.
+    :param lineno: The line number where rawtext appears in the input.
+    :param inliner: The inliner instance that called us.
+    :param options: Directive options for customization.
+    :param content: The directive content for customization.
+    '''
+    node = nodes.Text(' '.join([SPAM, text, SPAM]))
+    return [node], []
 
 
 class spam(nodes.Admonition, nodes.Element):
@@ -28,6 +48,9 @@ def depart_spam_node(self, node):
 
 
 class SpamDirective(Directive):
+    u'''Spamディレクティブ
+    '''
+
     # この変数をセットすると、ディレクティブの中に
     # コンテンツを書けるようになります。
     has_content = True
@@ -39,18 +62,20 @@ class SpamDirective(Directive):
                              self.content, self.lineno, self.content_offset,
                              self.block_text, self.state, self.state_machine)
 
-        if not hasattr(env, 'spam_all_spams'):
-            env.spam_all_spams = []
-        env.spam_all_spams.append({
+        spams = getattr(env, 'spam_all_spams', [])
+        spams.append({
             'docname': env.docname,
             'lineno': self.lineno,
             'spam': ad[0].deepcopy(),
         })
+        print(spams)
+        env.spam_all_spams = spams
 
         return ad
 
 
 def write_spam(app, doctree, fromdocname):
+    '''
     env = app.builder.env
     for spam_info in env.spam_all_spams:
         para = nodes.paragraph()
@@ -62,6 +87,8 @@ def write_spam(app, doctree, fromdocname):
         para += nodes.reference('', '')
         para += nodes.Text(SPAM)
         para += nodes.Text('.)', '.)')
+    '''
+    pass
 
 
 def setup(app):
@@ -69,8 +96,9 @@ def setup(app):
 
     see http://sphinx.shibu.jp/ext/tutorial.html
     '''
+    app.add_role('spam', spam_role)
     arg_tuple = (visit_spam_node, depart_spam_node)
-    app.add_node(spam,
-            html=arg_tuple, latex=arg_tuple, text=arg_tuple)
+    app.add_node(spam, html=arg_tuple, latex=arg_tuple,
+            text=arg_tuple)
     app.add_directive('spam', SpamDirective)
     app.connect("doctree-resolved", write_spam)
